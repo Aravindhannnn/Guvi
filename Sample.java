0d5dcecb52ff4072ebd1f737c1db3ef23c53317a
Aravindhan <virataravindhan@gmail.com>
2018-04-04 20:55:13 +0530

    Sample

--- Sample.java
+++ Sample.java
@@ -4,7 +4,8 @@ class Sample{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
 		if(n>0){
-		System.out.println("Positive")}
+		System.out.println("Positive");
+	}
 		else if(n==0){
 			System.out.println("Zero");
 		}
