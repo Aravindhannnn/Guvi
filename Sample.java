f3a93d1b07f0ecade0aedbcb8e72e29dc171328b
Aravindhan <virataravindhan@gmail.com>
2018-04-04 20:39:10 +0530

    My Java Code

---
+++ Sample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sample{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n>0){
+		System.out.println("Positive")}
+		else if(n==0){
+			System.out.println("Zero");
+		}
+		else{
+			System.out.println("Negative");
+		}
+	}
+}
